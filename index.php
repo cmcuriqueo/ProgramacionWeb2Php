278e1655144faa82bddd18008c9871fe2736233e
cmcuriqueo <cmcuriqueo@udc.edu.ar>
2016-08-24 19:02:45 -0300

    mas cosas

--- index.php
+++ index.php
@@ -55,6 +55,34 @@
         </div>
     </div>
 </form>
+<form class="form-horizontal" method="GET" action="db_connect.php" id="formulario_index">
+   <div class="container">
+        <div class="row-flow">
+            <p></p>
+            <div class="col-md-9">
+                <div class="panel panel-success">
+
+                    <div class="panel-body">Ver Clientes Mayores de: </div>
+
+                    <div class="panel-footer">
+                        <ul class="list-group">
+
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label" for="edad">Edad : </label>
+                                <div class="col-sm-10"> <input name="edad" type="number" placeholder="Edad"> </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-sm-offset-2 col-sm-10">
+                                    <button class="btn btn-default" type="submit">Enviar</button>
+                                </div>
+                            </div>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
 <?php
 require __DIR__ . '/footer.php';
 
